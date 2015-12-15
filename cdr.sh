3ad1f7f9406dfa78db7f7b30bb48a2ff23fe2347
win3 <win3@RAH.LOCAL>
2015-12-15 14:28:01 -0500

    remove sensitive info from cdr.sh

--- cdr.sh
+++ cdr.sh
@@ -5,6 +5,6 @@ CurrentYear=$(date -d "1 day ago" +"%Y")
 
 cd "/mnt/share/COMMON FILES/STAFF FOLDERS/Jeffrey Zic/CDRHourly"
 
-curl -H 'content-type: application/json' "192.168.0.199/gui/cdr/cdr?__auth_user=admin&__auth_pass=c^c\$g%o)d&sortby=end_timestamp&sortorder=desc&since=RANGE&rows=2000&between=${CurrentMonth}+${CurrentDay}%2C+${CurrentYear}&between=${CurrentMonth}+${CurrentDay}%2C+${CurrentYear}&show_outbound=0" > "/mnt/share/COMMON FILES/STAFF FOLDERS/Jeffrey Zic/CDRHourly/CDR/$NOW"
+curl -H 'content-type: application/json' "192.168.0.199/gui/cdr/cdr?__auth_user=****&__auth_pass=****d&sortby=end_timestamp&sortorder=desc&since=RANGE&rows=2000&between=${CurrentMonth}+${CurrentDay}%2C+${CurrentYear}&between=${CurrentMonth}+${CurrentDay}%2C+${CurrentYear}&show_outbound=0" > "/mnt/share/COMMON FILES/STAFF FOLDERS/Jeffrey Zic/CDRHourly/CDR/$NOW"
 
 echo "/mnt/share/COMMON FILES/STAFF FOLDERS/Jeffrey Zic/CDRHourly/CDR/$NOW" | python "/mnt/share/COMMON FILES/STAFF FOLDERS/Jeffrey Zic/CDRHourly/hourlycdr.py" 
