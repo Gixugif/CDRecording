e74fbac9974953bac07552e79a541064daf9e3aa
Jeffrey Zic <jeff@rhinebeckanimalhospital.com>
2016-08-05 09:59:05 -0400

    test: Add test for usernamePrompt rtype
    Add test to make sure usernamePrompt is returning a string.

--- CDR_Tests.py
+++ CDR_Tests.py
@@ -2,8 +2,14 @@ import unittest
 
 class Call_Detail_Directory_Tests(unittest.TestCase):
     
+    def usernamePromptReturnsString(self):
+        actual = type(usernamePrompt("un"))
+        expected = str
+
+        self.assertEqual(expected,actual)
+
     def getLoginFunctionReturnsString(self):
-        actual = type(getLogin())
+        actual = type('','',CudaRespose)
         expected = str
         
         self.assertEqual(true,all(isinstance(n,expected) for n in acutal))
